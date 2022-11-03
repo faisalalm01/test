0ecefafabc03f6893bd9666bd33b0a357cadf7e2
Muhammad Isa Nuruddin <75399939+muhammadisa226@users.noreply.github.com>
2022-11-03 20:49:38 +0700

    update web design and video creatife  competition

--- src/components/Competitions/Deslomba2.jsx
+++ src/components/Competitions/Deslomba2.jsx
@@ -13,10 +13,19 @@ export default function Deslomba2() {
                 DUNIA. TIDAK HANYA SUARA YANG SEKALIGUS BISA DITERIMA OLEH AUDIENS,
                 INFORMASI DAN PESAN AKAN LEBIH MUDAH DITAMPILKAN DENGAN GAMBAR.</p>
               <h3 className=' font-weight-bold'>Penjelasan Lomba </h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur, nesciunt dignissimos laborum doloribus ad earum totam repudiandae voluptatibus soluta?
-                  Illum doloremque ut voluptates quo?
-          Minus porro veniam maiores illum eligendi. Voluptates veniam reiciendis maxime eum sequi et ipsa deserunt quo.</p>
+              <h6>A.) KETENTUAN KOMPETISI</h6>
+              <p>&bull; VIDEO PENDEK BERDURASI MAKSIMAL 10 MENIT.</p>
+              <p>&bull; VIDEO DIBUAT PERORANGAN ATAU KELOMPOK.</p>
+              <p>&bull; TEMA YANG DIPILIH YAITU " SUASANA KEHIDUPAN KULIAH DI PRODI TEKNIK
+                        INFORMATIKA POLITEKNIK HARAPAN BERSAMA".</p>
+              <p>&bull; VIDEO TIDAK MENGANDUNG SARA DAN TIDAK MELANGGAR HUKUM/ ATURAN YANG
+BERLAKU.</p>
+              <p>&bull; VIDEO KREATIF YANG SUDAH JADI UPLOAD DI GOOGLE DRIVE</p>
+              <p>&bull; CANTUMKAN LOGO POLITEKNIK HARAPAN BERSAMADAN LOGO INVOFEST, LOGO BISA
+                        UNDUH LINK DIBAWAH INI:  <a href="https://drive.google.com/drive/folders/1zCcJUy3zWBKoi2LhSB9JkbTaaDF6LGd?usp=sharing">https://drive.google.com/drive/folders/1zCcJUy3zWBKoi2LhSB9JkbTaaDF6LGd?usp=sharing</a></p>
+              <p>&bull; VIDEO YANG SUDAH JADI DIUPLOAD DI GOOGLE DRIVE DAN LINK DRIVE DIKUMPULKAN
+                        PADA FORM YANG TERSEDIA</p>
+              <p>&bull; LINK PENGUMPULAN : https://bit.ly/m/INVOFEST2022</p>
           <Button href='https://drive.google.com/file/d/16pYqsmR-YSN3-HOL5xvfD0FuPKbZU7lY/view?usp=share_link' className='btn btn-primary'>Guide book</Button>
 
         
--- src/components/Competitions/Reg3.jsx
+++ src/components/Competitions/Reg3.jsx
@@ -3,16 +3,21 @@ import React from "react";
 export default function Reg3() {
   return (
     <div>
-      <div className="container mt-5">
+        <div className="container mt-5">
         <h3>Info Registrasi</h3>
-        <p>Biaya Registrasi : 150.00-/tim</p>
+        <p>Biaya Registrasi : RP. 150.000/Tim</p>
         <p>Pembayaran via transfer:</p>
-        <p>No Rekening -</p>
+        <p>- MANDIRI : 1390026107429 (A/N AHMAD FAOZAN) </p>
+        <p>- OVO : 0821 3576 4818 (A/N AHMAD FAOZAN)</p>
+        <p>- DANA : 0821 3576 4818 (A/N AHMAD FAOZAN)</p>
+        <p>- SHOPEE PAY : 0821 3576 4818 (A/N AHMAD FAOZAN)</p>
+        <p>- GOPAY : 0821 3576 4818 (A/N AHMAD FAOZAN)</p>
         <p>Konfirmasi Pembayaran:</p>
-        <p>Wa : -</p>
+        <p>Wa : - </p>
         <p>email : - </p>
         <h3>Contact Person</h3>
-        <p>-</p>
+        <p>- 085225778051 (TRI HESTI WAHYUNINGSIH)</p>
+        <p>- 082314923655 (FAJAR SIDIQ)</p>
       </div>
     </div>
   );
