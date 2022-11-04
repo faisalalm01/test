ff131bb6802b78ad2a4a7d2267925d0c8ea465e2
faisalalm01 <faisalali2858@gmail.com>
2022-11-04 17:14:13 +0700

    v2.9

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,68 +3,68 @@ import React from 'react'
 export default function Timeline() {
     return (
         <div>
-            <div class="timeline">
-                <div class="timeline__component">
-                    <div class="timeline__date timeline__date--right">1 - 25 November, 2022</div>
+            <div className="timeline">
+                <div className="timeline__component">
+                    <div className="timeline__date timeline__date--right">1 - 25 November, 2022</div>
                 </div>
-                <div class="timeline__middle">
-                    <div class="timeline__point"></div>
+                <div className="timeline__middle">
+                    <div className="timeline__point"></div>
                 </div>
-                <div class="timeline__component timeline__component--bg">
-                    <h2 class="timeline__title">Pendaftaran</h2>
-                    <p class="timeline__paragraph">
+                <div className="timeline__component timeline__component--bg">
+                    <h2 className="timeline__title">Pendaftaran</h2>
+                    <p className="timeline__paragraph">
                         <li>Seminar</li>
                         <li>Talkshow</li>
                         <li>Workshop</li>
                         <li>Competition</li>
                     </p>
-                    <h2 class="timeline__title">Pengumpulan</h2>
-                    <p class="timeline_paragraph">
+                    <h2 className="timeline__title">Pengumpulan</h2>
+                    <p className="timeline_paragraph">
                         <li>Competition(25 November, 2022)</li>
                     </p>
                 </div>
-                <div class="timeline__component timeline__component--bg">
-                    <h2 class="timeline__title"></h2>
-                    <p class="timeline__paragraph">
+                <div className="timeline__component timeline__component--bg">
+                    <h2 className="timeline__title"></h2>
+                    <p className="timeline__paragraph">
 
                     </p>
-                    <p class="timeline__paragraph">
-
-                    </p>
-                </div>
-                <div class="timeline__middle">
-                    <div class="timeline__point"></div>
-                </div>
-                <div class="timeline__component">
-                    <div class="timeline__date">November 29, 2022</div>
-                </div>
-                <div class="timeline__component">
-                    <div class="timeline__date timeline__date--right">November 30, 2022</div>
-                </div>
-                <div class="timeline__middle">
-                    <div class="timeline__point"></div>
-                </div>
-                <div class="timeline__component timeline__component--bg">
-                    <h2 class="timeline__title">a</h2>
-                    <p class="timeline__paragraph">
+                    <p className="timeline__paragraph">
 
                     </p>
                 </div>
-                <div class="timeline__component timeline__component--bottom timeline__component--bg">
-                    <h2 class="timeline__title">w</h2>
-                    <p class="timeline__paragraph">
-
-                    </p>
-                    <p class="timeline__paragraph">
+                <div className="timeline__middle">
+                    <div className="timeline__point"></div>
+                </div>
+                <div className="timeline__component">
+                    <div className="timeline__date">November 29, 2022</div>
+                </div>
+                <div className="timeline__component">
+                    <div className="timeline__date timeline__date--right">November 30, 2022</div>
+                </div>
+                <div className="timeline__middle">
+                    <div className="timeline__point"></div>
+                </div>
+                <div className="timeline__component timeline__component--bg">
+                    <h2 className="timeline__title">a</h2>
+                    <p className="timeline__paragraph">
 
                     </p>
                 </div>
-                <div class="timeline__middle">
-                    <div class="timeline__point"></div>
-                    <div class="timeline__point timeline__point--bottom"></div>
+                <div className="timeline__component timeline__component--bottom timeline__component--bg">
+                    <h2 className="timeline__title">w</h2>
+                    <p className="timeline__paragraph">
+
+                    </p>
+                    <p className="timeline__paragraph">
+
+                    </p>
                 </div>
-                <div class="timeline__component timeline__component--bottom">
-                    <div class="timeline__date">Desember 1, 2022</div>
+                <div className="timeline__middle">
+                    <div className="timeline__point"></div>
+                    <div className="timeline__point timeline__point--bottom"></div>
+                </div>
+                <div className="timeline__component timeline__component--bottom">
+                    <div className="timeline__date">Desember 1, 2022</div>
                 </div>
             </div>
         </div>
