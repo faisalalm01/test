d004b625a65b4785b4f16bf90c7d9fdafc0b6d69
faisalalm01 <faisalali2858@gmail.com>
2022-11-08 10:38:21 +0700

    v2.9

--- src/pages/Competition/Competition.jsx
+++ src/pages/Competition/Competition.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from "react";
 import { Row, Container, Col } from "react-bootstrap";
-import Particles from "react-tsparticles";
 import Cards from "../../components/Competitions/Cards";
 import CardAcara from "../../components/LandingPage/CardBanner";
 import Navbars from "../../components/Navbar";
 import Footer from '../../components/Footer'
+import Animate from "react-smooth/lib/Animate";
+import Particle from "../../components/Particles";
 
 
 class Competition extends Component {
   render() {
     return (
       <div>
-        <Navbars />
-        <Particles />
-        <Container className="App-header" fluid={true}>
-          <Row className="App-main">
-            <Col xl={12} className="App-center mt-5">
-              <CardAcara />
-            </Col>
-          </Row>
-        </Container>
+        <Animate to="1" from="0" attributeName="opacity">
+          <Particle />
+          <Navbars />
+          <Container className="App-header" fluid={true}>
+            <Row className="App-main">
+              <Col xl={12} className="App-center mt-5">
+                <CardAcara />
+              </Col>
+            </Row>
+          </Container>
+        </Animate>
         <Cards />
         <footer className="mt-5">
           <Footer />
